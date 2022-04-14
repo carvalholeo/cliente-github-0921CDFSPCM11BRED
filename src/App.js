95c15ebc421b40257b037580a90ff60691c8dd0f
Léo Carvalho <carvalho.csleo@gmail.com>
2022-04-14 11:43:55 -0300

    Implementa conteúdo e navegação no App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 // const logo = require('./logo.svg');
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import BemVindo from './components/BemVindo';
+import NavBar from './components/NavBar'
 
 function App() {
+  const caminhos = [
+    {
+      id: 1,
+      caminho: '/',
+      titulo: 'Página inicial'
+    },
+    {
+      id: 2,
+      caminho: '/perfil',
+      titulo: 'Perfil'
+    },
+    {
+      id: 3,
+      caminho: '/repositorios',
+      titulo: 'Repositórios'
+    },
+    {
+      id: 4,
+      caminho: '/busca',
+      titulo: 'Busca'
+    }
+  ]
   return (
-    <div>
+    <div >
+      <NavBar menu={caminhos}  />
+      <BemVindo meuNome="Léo"></BemVindo>
+      <BemVindo meuNome="Lígia"/>
+      <BemVindo />
+      <BemVindo />
+      <BemVindo />
+      <BemVindo />
     </div>
   );
 }
