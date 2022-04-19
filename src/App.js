4bbb4d1e516e3fb9687ec246337e248dec11668a
Léo Carvalho <carvalho.csleo@gmail.com>
2022-04-19 11:08:22 -0300

    Instala e configura axios, limpa App.js e cria tela de Perfil

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-// const logo = require('./logo.svg');
-import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import BemVindo from './components/BemVindo';
 import NavBar from './components/NavBar'
 
 function App() {
@@ -29,32 +26,11 @@ function App() {
       titulo: 'Busca'
     }
   ]
-  const [numero, setNumero] = useState(1)
 
-  useEffect(() => {
-    alert('O número foi alterado')
-    // setNumero(numero + 1)
-  }, [numero])
-
-  useEffect(() => {
-    setNumero(numero + 1)
-  }, [])
-
-  function aumentaNumero() {
-    setNumero(numero+numero)
-
-    console.log(numero)
-  }
   return (
     <div >
       <NavBar menu={caminhos}  />
-      <h1>{numero}</h1> <button onClick={aumentaNumero}>Clique aqui</button>
-      <BemVindo meuNome="Léo"></BemVindo>
-      <BemVindo meuNome="Lígia"/>
 
-      {numero}
-    <br />
-      {numero}
     </div>
   );
 }
